5693c13163b42e918f1f816c98dc47d67c6650f4
Xin Su <xinsu@Xins-MacBook-Pro.local>
2018-08-01 20:47:50 -0700

    First commit: create plustut project server with node.js

---
+++ config/default.js
@@ -0,0 +1,11 @@
+module.exports = {
+	port:3000,
+	database:{
+		DATABASE: 'plusTuT',
+	    USER: 'root',
+	    PASSWORD: 'catherine100529',
+		HOST: 'localhost',
+		PORT: 3306
+	},
+	jwt_secret: 'ddff0a63e06816ddd7b7d2e2ebc1e40205'
+}
