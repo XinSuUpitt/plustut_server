94a697c848f6d37c9cdb0b945dab02dc49836d8e
Xin Su <xinsu@Xins-MacBook-Pro.local>
2018-08-05 20:38:02 -0700

    Try to point to aws rds

--- config/default.js
+++ config/default.js
@@ -1,10 +1,10 @@
 module.exports = {
 	port:80,
 	database:{
-		DATABASE: 'plusTuT',
-	    USER: 'root',
+		DATABASE: 'plustut',
+	    USER: 'xinsu0608',
 	    PASSWORD: 'catherine100529',
-		HOST: 'localhost',
+		HOST: 'plustut.cxsxaix1kweg.ap-northeast-2.rds.amazonaws.com',
 		PORT: 3306
 	},
 	jwt_secret: 'ddff0a63e06816ddd7b7d2e2ebc1e40205'
