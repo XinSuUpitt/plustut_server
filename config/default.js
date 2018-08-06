486c46d9cc22415dec9dea0106f60eb2e2f43e55
Xin Su <xinsu@Xins-MacBook-Pro.local>
2018-08-05 21:04:07 -0700

    Change DB

--- config/default.js
+++ config/default.js
@@ -1,10 +1,10 @@
 module.exports = {
 	port:80,
 	database:{
-		DATABASE: 'plustut',
-	    USER: 'plustut',
+		DATABASE: 'aa13z0c4z1pljnz',
+	    USER: 'xinsu',
 	    PASSWORD: 'catherine100529',
-		HOST: 'plustut.cxsxaix1kweg.ap-northeast-2.rds.amazonaws.com',
+		HOST: 'aa13z0c4z1pljnz.cxsxaix1kweg.ap-northeast-2.rds.amazonaws.com',
 		PORT: 3306
 	},
 	jwt_secret: 'ddff0a63e06816ddd7b7d2e2ebc1e40205'
