fe282264bd419ae9ee5ae35e59f7fae6b9cad89e
Xin Su <xinsu@Xins-MacBook-Pro.local>
2018-08-05 20:41:36 -0700

    Try again

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ module.exports = {
 	port:80,
 	database:{
 		DATABASE: 'plustut',
-	    USER: 'xinsu0608',
+	    USER: 'plustut',
 	    PASSWORD: 'catherine100529',
 		HOST: 'plustut.cxsxaix1kweg.ap-northeast-2.rds.amazonaws.com',
 		PORT: 3306
