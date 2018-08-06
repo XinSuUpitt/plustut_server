4d9144c6bf01915b718bfd6df0d6f7c143dfad2d
Xin Su <xinsu@Xins-MacBook-Pro.local>
2018-08-05 21:25:00 -0700

    Fix typo

--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ module.exports = {
 	    USER: 'xinsu',
 	    PASSWORD: 'catherine100529',
 		HOST: 'aa13z0c4z1pljnz.cxsxaix1kweg.ap-northeast-2.rds.amazonaws.com',
-		PORT: 3306
+		PORT: '3306'
 	},
 	jwt_secret: 'ddff0a63e06816ddd7b7d2e2ebc1e40205'
 }
