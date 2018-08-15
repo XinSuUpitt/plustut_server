db7214dc11fed91012032ef72c9e5aa29b7f56f0
Xin Su <xinsu@Xins-MacBook-Pro.local>
2018-08-15 02:15:49 -0700

    Add SSL 4

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 
-var privateKey  = fs.readFileSync('~/sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('~/sslcert/server.crt', 'utf8');
-var ca = [ fs.readFileSync('~/sslcert/intermediate.crt', 'utf8') ];
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var ca = [ fs.readFileSync('sslcert/intermediate.crt', 'utf8') ];
 var credentials = {ca: ca, key: privateKey, cert: certificate};
 
 var routes = require('./router')(app);
